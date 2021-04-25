0e1a12e74fdb3217fad401cce9c69ac68d2dca5b
rito15 <ghdtkals15@gmail.com>
2021-04-25 20:09:14 +0900

    Revert "Revert "Bresenham Algorithm""
    This reverts commit 8f04198b6debc9d32a6a7b6ddd0c2515cd70f682.

--- CSharp Algorithm study/CSharp Algorithm study/MainClass.cs
+++ CSharp Algorithm study/CSharp Algorithm study/MainClass.cs
@@ -12,7 +12,45 @@ namespace CSharp_Algorithm_study
     {
         public static void Main()
         {
-            int[] arr = { 10, 2, 7, 8, 6, 7, 2, 0, 1, 3 };
+            // 축
+            //BresenhamAlgorithm.Run((0,0), (15, 0));  // +x
+            //BresenhamAlgorithm.Run((0,0), (-15, 0)); // -x
+            //BresenhamAlgorithm.Run((0,0), (0, 15));  // +y
+            //BresenhamAlgorithm.Run((0,0), (0, -15)); // -y
+
+            // 1사분면
+            //BresenhamAlgorithm.Run((0,0), (15, 15));
+            //BresenhamAlgorithm.Run((0,0), (15, 4));
+            //BresenhamAlgorithm.Run((0,0), (4, 15));
+
+            // 2사분면
+            //BresenhamAlgorithm.Run((0,0), (-15, 15));
+            //BresenhamAlgorithm.Run((0,0), (-4, 15));
+            //BresenhamAlgorithm.Run((0,0), (-15, 4));
+
+            // 3사분면
+            //BresenhamAlgorithm.Run((0,0), (-15, -15));
+            //BresenhamAlgorithm.Run((0,0), (-4, -15));
+            //BresenhamAlgorithm.Run((0,0), (-15, -4));
+
+            // 4사분면
+            //BresenhamAlgorithm.Run((0,0), (15, -15));
+            //BresenhamAlgorithm.Run((0,0), (4, -15));
+            //BresenhamAlgorithm.Run((0,0), (15, -4));
+
+            Bresenham bh = new Bresenham((-5, 2), (0, 15));
+            //foreach (var point in bh)
+            //    Console.WriteLine(point);
+
+            for (int i = 0; i < bh.Count; i++)
+            {
+                Console.WriteLine(bh[i]);
+            }
+        }
+
+        public static void Main_Sort()
+        {
+            int[] arr = { 9, 2, 7, 8, 6, 7, 2, 0, 1, 3 };
             //int[] arr = { 1, 2, 2, 1, 2, 0, 1, 2, 0 };
             int[] orgArr = new int[arr.Length];
             arr.CopyTo(orgArr, 0);
@@ -20,10 +58,10 @@ namespace CSharp_Algorithm_study
             //new BubbleSort().   Sort(arr);
             //new SelectionSort().Sort(arr);
             //new InsertionSort().Sort(arr);
-            //new QuickSort().    Sort(arr);
+            new QuickSort().    Sort(arr);
             //new MergeSort().    Sort(arr);
             //new HeapSort().     Sort(arr);
-            new CountingSort(). Sort(arr);
+            //new CountingSort(). Sort(arr);
 
             ShowArray(orgArr);
             ShowArray(arr);
@@ -31,7 +69,6 @@ namespace CSharp_Algorithm_study
 
             void ShowArray<T>(T[] array)
             {
-
                 for (int i = 0; i < array.Length; i++)
                     Console.Write($"{array[i]} ");
                 Console.WriteLine();
